daca0523f9020ec1f3d63f6223b5fe97f2acc43e
Concrete-Slab <thomaswilliams3982@gmail.com>
2023-11-26 01:15:23 +0000

    Improved threadsafety with SharedState

--- support_classes/shared_state.py
+++ support_classes/shared_state.py
@@ -1,5 +1,5 @@
 from typing import TypeVar, Generic
-from threading import Event
+from threading import Event, Lock
 
 T = TypeVar("T")
 class SharedState(Generic[T]):
@@ -7,15 +7,18 @@ class SharedState(Generic[T]):
     def __init__(self,initialValue: T=None) -> None:
         self.value: T | None = initialValue
         self.__event = Event()
+        self.__lock = Lock()
 
     def set_value(self,value: T):
-        self.value = value
-        self.__event.set()
+        with self.__lock:
+            self.value = value
+            self.__event.set()
 
     def get_value(self) -> T | None:
-        if self.__event.is_set():
-            self.__event.clear()
-            return self.value
-        else:
-            return None
-
+        with self.__lock:
+            if self.__event.is_set():
+                self.__event.clear()
+                val = self.value
+                return val
+            else:
+                return None
