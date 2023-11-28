74983e87116a1f1b0c54608bb306660c22c24720
Concrete-Slab <thomaswilliams3982@gmail.com>
2023-11-28 15:32:25 +0000

    Added generic image capture function

--- support_classes/__init__.py
+++ support_classes/__init__.py
@@ -3,4 +3,4 @@ from .Teardown import Teardown, TDExecutor
 from .AsyncRunner import AsyncRunner
 from .Generator import Generator, GeneratorException
 from .shared_state import SharedState
-from .context_managers import open_cv2_window, open_video_device
+from .context_managers import open_cv2_window, open_video_device, capture, CaptureException
--- support_classes/context_managers.py
+++ support_classes/context_managers.py
@@ -1,5 +1,7 @@
 from contextlib import contextmanager
 import cv2
+from typing import Any
+import numpy as np
 
 @contextmanager
 def open_video_device(index: int):
@@ -16,3 +18,19 @@ def open_cv2_window(name: str):
         yield name
     finally:
         cv2.destroyWindow(name)
+
+def capture(arg0: Any) -> np.array:
+    if isinstance(arg0,str):
+        cap = cv2.imread(arg0)
+        return cap
+    if isinstance(arg0,int):
+        with open_video_device(arg0) as vc:
+            is_retrieved, cap = vc.read()
+        if not is_retrieved:
+            raise CaptureException("Image not retrieved")
+        return cap
+    raise TypeError("Capture argument must be int or str")
+
+class CaptureException(BaseException):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
