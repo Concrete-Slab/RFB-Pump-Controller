62e9e7eed3cd9549482994f8ab314c679839e998
Concrete-Slab <thomaswilliams3982@gmail.com>
2024-01-03 18:43:50 +0000

    Made DummyInterface speed readings reflect duty writes

--- serial_interface/DummyInterface.py
+++ serial_interface/DummyInterface.py
@@ -1,4 +1,5 @@
 from .GenericInterface import GenericInterface, InterfaceException
+from support_classes import PumpNames
 import asyncio
 import random
 
@@ -8,22 +9,28 @@ class DummyInterface(GenericInterface):
     def __init__(self,port,**kwargs):
         super().__init__(port,**kwargs)
         self.port=port
+        self.applied_duties = {pmp:0 for pmp in PumpNames}
 
     async def establish(self):
         await asyncio.sleep(1.0)
-        # raise InterfaceException("HAHAHAHAHAHAHA")
 
     async def readbuffer(self) -> str:
         await asyncio.sleep(1)
-        # raise InterfaceException
         out = [""]*6
-        for i in range(0,6):
-            out[i] = str(random.random()*12300)
+        for i,duty in enumerate(self.applied_duties.values):
+            out[i] = str(int(duty/255*12300) + random.random()*100)
         outstr = ",".join(out)
         return outstr
     
     async def write(self,val: str):
         await asyncio.sleep(0.1)
+        
+        try:
+            pmp = PumpNames(val[1])
+            duty = int(val[3])
+            self.applied_duties[pmp] = duty
+        except:
+            pass
         print("Writing",val)
 
     def close(self):
