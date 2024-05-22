32ad94934a066aa916b73bcf3c0cc232313f7275
Concrete-Slab <thomaswilliams3982@gmail.com>
2024-05-22 17:05:12 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pip",
+    name="rfb-controller",
     version="0.1.0",
     author="Thomas Williams",
     author_email="thomaswilliams3982@gmail.com",
